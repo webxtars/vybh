f63677b17025a8ca4eb8ed572efd4a94f8dea5b5
David Godswill <davidilerioluwa1998@gmail.com>
2025-04-19 19:24:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-blue-800 text-5xl'>Welcome to The VYBH Homepage</div>
+       
+    </>
+  )
+}
+
+export default App
