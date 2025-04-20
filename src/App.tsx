725bd6e49532c0b7131a91440a9f8d26bc75e446
David Godswill <davidilerioluwa1998@gmail.com>
2025-04-20 19:56:50 +0100

    fixing commit issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <div className='h-screen w-screen flex items-center justify-center'>
-      <div className='text-blue-800 text-5xl '>Welcome to The VYBH Homepage</div> 
+      <div className='text-blue-800 text-5xl '>Welcome to The VYBH Homepage. Testing for pull request</div> 
     </div>
   )
 }
