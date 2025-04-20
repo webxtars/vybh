993ac139c0afd3bf58bc525df6f08ca6a3bf2fa7
David Godswill <davidilerioluwa1998@gmail.com>
2025-04-20 17:26:56 +0100

    fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='text-blue-800 text-5xl'>Welcome to The VYBH Homepage</div>
-       
-    </>
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className='text-blue-800 text-5xl '>Welcome to The VYBH Homepage</div> 
+    </div>
   )
 }
 
