abc42ab89a7a728426e152c1d6d547eb19081d47
David Godswill <davidilerioluwa1998@gmail.com>
2025-05-08 19:29:43 +0100

    created events page

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -4,7 +4,7 @@ const EventCard = () => {
   return (
     <Link to="/event" className='flex flex-col w-80 cursor-pointer'>
     <img src='image.png' className='rounded-t-lg h-60 object-cover'/>
-    <div className='bg-gray rounded-b-lg flex flex-col gap-2 p-4'>
+    <div className='bg-lightgray rounded-b-lg flex flex-col gap-2 p-4'>
       <h2 className='font-bold'>Crypto Beats Festival</h2>
       <div className='flex justify-between text-xs '>
         <span className='flex items-center gap-1.5'><span><FaRegCalendar/></span>Mar 15, 2025</span>
