46dd30fcc3c50c5a88325d91f13104d1ba28a49a
David Godswill <davidilerioluwa1998@gmail.com>
2025-05-20 21:08:25 +0100

    increased font size on landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <div className='h-[400px] mt-16 md:mt-0 md:h-screen flex items-center justify-center'>
       <img className='h-[400px] md:h-screen w-screen object-cover absolute z-0' src={"landing.jpg"}/>
       <div className=' z-10 text-white text-center flex flex-col gap-4 items-center px-6'>
-        <h1 className='text-xl md:text-3xl lg:text-6xl font-bold gradient-animation slideRightIn  bg-clip-text  text-transparent  bg-gradient-to-p from-purple to-white'>EXPERIENCE WEB-3 EVENTS</h1>
+        <h1 className='text-3xl lg:text-6xl font-bold gradient-animation slideRightIn  bg-clip-text  text-transparent  bg-gradient-to-p from-purple to-white'>EXPERIENCE WEB-3 EVENTS</h1>
         <p className='text-md md:text-xl lg:text-2xl font-bold'>Secure your NFT tickets for exclusive concerts, shows, and experiences</p>
         <Button onClick={()=>navigate("/events")} color='primary' variant='solid' title='Discover Events' size='large'/>
       </div>
